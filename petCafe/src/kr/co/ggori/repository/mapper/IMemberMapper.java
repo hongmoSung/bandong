1ff6754ab9147b729f302ae7c4a66af474ef14f8
fbehdrl <comboy5419@naver.com>
2017-06-15 21:32:29 +0900

    회원등급

--- petCafe/src/kr/co/ggori/repository/mapper/IMemberMapper.java
+++ petCafe/src/kr/co/ggori/repository/mapper/IMemberMapper.java
@@ -9,7 +9,9 @@ public interface IMemberMapper {
 	public int insertMember(MemberVO member) throws Exception;
 	public MemberVO selectMemberOne(MemberVO member) throws Exception;
 //	public String selectMemberByEmail(String email) throws Exception;
+	public List<MemberVO> selectMemberAll () throws Exception;
 	public List<MemberVO> selectMemberByEmail(String email) throws Exception;
 	public int updateMemberPass(MemberVO member) throws Exception;
 	public int updateMember(MemberVO member) throws Exception;
+	public int updateMemberRank(MemberVO member) throws Exception;
 }
